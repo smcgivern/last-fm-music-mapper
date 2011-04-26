dce66709a879d4924ab42641cc073dcd7f89489e
Sean McGivern <sean@mcgivern.me.uk>
2011-04-26 17:09:43 +0100

    Move thematic mapping config out of ::Config

--- lib/thematic_map.rb
+++ lib/thematic_map.rb
@@ -5,24 +5,27 @@ require 'nokogiri'
 # the colour scale and magnitudes given. Only linear colour scales are
 # supported at present, not divergent or
 module ThematicMap
-  class Config
+  class << self
     @@map_file = :world_compact
     @@output_format = 'svg'
     @@output_filename = 'thematic_map'
 
-    @@colour_palette = []
+    # http://www.personal.psu.edu/cab38/ColorBrewer/ColorBrewer_RGB.html
+    @@colour_palette = ['#000000']
 
-    # Create accessors for class variables (used by all subclasses, so
-    # can be set anywhere.
-    class_variables.each do |var|
-      # Provide aliases for colour / color variation.
-      methods = [var, var.gsub('colour', 'color')].uniq.map {|x| x[2..-1]}
+    def create_map; self.colour_palette; end
+  end
 
+  # Create accessors for class variables (used by all subclasses, so
+  # can be set anywhere.
+  class_variables.each do |var|
+    # Provide aliases for colour / color variation.
+    methods = [var, var.gsub('colour', 'color')].uniq.map {|x| x[2..-1]}
+
+    methods.each do |meth|
       (class << self; self; end).class_eval do
-        methods.each do |meth|
-          define_method(meth) {class_variable_get(var)}
-          define_method("#{meth}=") {|x| class_variable_set(var, x)}
-        end
+        self.send(:define_method, meth) {class_variable_get(var)}
+        define_method("#{meth}=") {|x| class_variable_set(var, x)}
       end
     end
   end
