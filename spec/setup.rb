75efb9fa249c0a25991408b97e2087b4e54b46a0
Sean McGivern <sean@mcgivern.me.uk>
2012-09-27 12:21:45 +0200

    Do DataMapper logging in specs

--- spec/setup.rb
+++ spec/setup.rb
@@ -1,6 +1,10 @@
 require 'bacon'
 require 'bundler/setup'
 
-SETTINGS = {'database' => {'url' => 'sqlite::memory:'}}
+SETTINGS = {'database' => {
+    'url' => 'sqlite::memory:',
+    'log_file' => "tmp/spec-datamapper.log",
+    'log_level' => 'debug',
+  }}
 
 require './setup'
