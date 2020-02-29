02ea2bdf1a71536f18180b3d648a09b33048fb0a
Sean McGivern <sean@mcgivern.me.uk>
2020-02-29 14:47:23 +0000

    Add basic index template

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+module Main (main) where
+
+import Data.Aeson
+import GHC.Generics
+import Text.Mustache
 import Web.Scotty
+
+import qualified Text.Mustache.Compile.TH as TH
+
+data IndexPage = IndexPage
+  { pageTitle :: String
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON IndexPage
 
 main = scotty 3000 $ do
   get "/" $ do
-    html "Hello World!"
+    username <- param "username" `rescue` (\_ -> next)
+    redirect $ mconcat ["/:", username]
+  get "/" $ do
+    let template = $(TH.compileMustacheFile "./template/index.html")
+    let indexPage = IndexPage { pageTitle = "Last.fm music mapper" }
+    html $ renderMustache template (toJSON indexPage)
+  get (regex "^/:([^/]+)/?$") $ do
+    username <- param "1"
+    html $ mconcat ["<h1>Hello, ", username, "!</h1>"]
